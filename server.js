716eb239ec458dfebbb98f3eed08c3405b0b20d9
julioborgesigt <julio.aparecido3@gmail.com>
2026-02-20 21:01:55 -0300

    corrige dependencias do teste e libera Kaspersky

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(helmet({
   contentSecurityPolicy: {
     directives: {
       defaultSrc: ["'self'"],
-      scriptSrc: ["'self'"],
+      scriptSrc: ["'self'", "https://*.kaspersky-labs.com"],
       styleSrc: [
         "'self'",
         "'unsafe-inline'",
